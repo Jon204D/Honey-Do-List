89e40fbddeb934d766514b9864e2dafad73e4d01
BrassMonkies <klh103199@outlook.com>
2025-10-09 10:12:19 -0400

    modified:   client/src/App.tsx 	modified:   client/src/Components/Navigation/NavigationBar.tsx 	modified:   client/src/Components/Profile/ProfileLogo.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,23 @@ import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 import Settings from "./Pages/Settings";
 import ForgotPassword from "./Pages/ForgotPassword";
+import NavigationBar from './Components/Navigation/NavigationBar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import InvitesPage from './Pages/Invite';
 import Tasks from "./Pages/Tasks";
-import NavigationBar from "./Components/Navigation/NavigationBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <NavigationBar/>
       <Routes>
-        <Route path="/" element={<Tasks/>} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/settings" element={<Settings/>} />
-        <Route path="/signup" element={<SignUp/>} />
+        <Route element={<NavigationBar/>}>
+          <Route path="/" element={<Tasks/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/settings" element={<Settings/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/invite" element={<InvitesPage/>} />
+        </Route>
       </Routes>
     </Router>
   );
