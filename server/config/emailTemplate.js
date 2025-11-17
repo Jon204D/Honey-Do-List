55ebc67b96fec452c4a650118dd3eb9e2a3d7178
Paityn Johnston <pjohnston2021@fau.edu>
2025-11-17 16:18:23 -0500

    fixed email template issue

--- server/config/emailTemplate.js
+++ server/config/emailTemplate.js
@@ -5,52 +5,46 @@ sgMail.setApiKey(sendGridAPI);
 const businessEmail = 'jdameus2025@fau.edu';
 const businessPOCEmail = 'jonathan.dameus@emerge-it.net';
 
-function sendVerification(email, username) {
-    const sentEmail = false;
-    const msg = {
-    to: email,
-    from: {name:'Honey Do List', email: businessEmail},
-    subject: 'Honey Do List Account Activation',
-    html: '<h1 style="font-size: 22px; font-family: Montserrat, sans-serif;">Hello ' + username.toLowerCase() + ', </h1>' + 
-    '\n\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">Thank you for registering with "Honey Do List".</p>' +
-    '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">Kindly click "Verify my account" below to verify your account.</p>' +
-    '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;"><a href="'+ process.env.FRONTEND_BASE_URL + "/AccountVerification/" + username.toLowerCase() + '">Verify my account</a></p>'
-    }
-    sgMail
-    .send(msg)
-    .then(() => {
-        sentEmail = true;
-        return;
-    })
-    .catch(error => {
-        console.error(error);
-        return;
-    });
-    return sentEmail;
-}
-
-function sendRecoveryVerification(email, username) {
-    sentEmail = false;
-    const msg = {
+async function sendVerification(email, username) {
+    try {
+        const msg = {
         to: email,
         from: {name:'Honey Do List', email: businessEmail},
-        subject: 'Honey Do List Account Recovery',
+        subject: 'Honey Do List Account Activation',
         html: '<h1 style="font-size: 22px; font-family: Montserrat, sans-serif;">Hello ' + username.toLowerCase() + ', </h1>' + 
-        '\n\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">This email is to provide you with your Honey Do List recovery link.</p>' +
-        '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">Kindly click "Recover my account" below to verify your account.</p>' +
-        '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;"><a href="'+ process.env.FRONTEND_BASE_URL + '/ForgotPasswordVerification/' + username.toLowerCase() + '">Recover my account</a></p>'
-    }
-    sgMail
-    .send(msg)
-    .then(() => {
-        sentEmail = true;
-        return;
-    })
-    .catch(error => {
+        '\n\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">Thank you for registering with "Honey Do List".</p>' +
+        '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">Kindly click "Verify my account" below to verify your account.</p>' +
+        '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;"><a href="'+ process.env.FRONTEND_BASE_URL + "/AccountVerification/" + username.toLowerCase() + '">Verify my account</a></p>'
+        };
+
+        await sgMail.send(msg);
+        
+        return true; // email was sent
+    } catch (error) {
         console.error(error);
-        return;
-    });
-    return sentEmail;
+        return false; //email was not sent
+    }
+}
+
+async function sendRecoveryVerification(email, username) {
+    try {
+        const msg = {
+            to: email,
+            from: {name:'Honey Do List', email: businessEmail},
+            subject: 'Honey Do List Account Recovery',
+            html: '<h1 style="font-size: 22px; font-family: Montserrat, sans-serif;">Hello ' + username.toLowerCase() + ', </h1>' + 
+            '\n\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">This email is to provide you with your Honey Do List recovery link.</p>' +
+            '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;">Kindly click "Recover my account" below to verify your account.</p>' +
+            '\n<p style="font-size: 16px; font-family: Montserrat, sans-serif;"><a href="'+ process.env.FRONTEND_BASE_URL + '/ForgotPasswordVerification/' + username.toLowerCase() + '">Recover my account</a></p>'
+        }
+        
+        await sgMail.send(msg);
+
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
 }
 
 module.exports = {
