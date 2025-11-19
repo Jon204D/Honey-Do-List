077b914afb9070b86ea44866d7ecff3d12f6b139
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2025-11-18 22:13:48 -0500

    Update production environment check logic
    The isProd function now returns false if HOST is 'localhost' or not equal to the production URL, improving accuracy of environment detection.

--- server/function/enviornmentCheck.js
+++ server/function/enviornmentCheck.js
@@ -1,5 +1,5 @@
 async function isProd() {
-    if (process.env.HOST.includes('localhost')) {
+    if (process.env.HOST.includes('localhost') || process.env.HOST !== 'https://honey-do-list-be-develop-production.up.railway.app') {
         return false;
     }
     return true;
